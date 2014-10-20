3d40828e343f83275d3a4391a68951045c33a42e
Alexey Zhokhov <donbeave@gmail.com>
2014-10-20 16:48:12 +0800

    Authors.

--- GwtGrailsPlugin.groovy
+++ GwtGrailsPlugin.groovy
@@ -14,30 +14,38 @@
  * limitations under the License.
  */
 
+/**
+ * @author <a href='mailto:david.dawson@simplicityitself.com'>David Dawson</a>
+ * @author <a href='mailto:p.ledbrook@cacoethes.co.uk'>Peter Ledbrook</a>
+ * @author <a href='mailto:donbeave@gmail.com'>Alexey Zhokhov</a>
+ */
 class GwtGrailsPlugin {
 
     def version = '2.0-SNAPSHOT'
     def grailsVersion = '2.3 > *'
 
     def title = 'The Google Web Toolkit for Grails.'
+    def author = 'David Dawson, Peter Ledbrook, Alexey Zhokhov'
+    def authorEmail = 'donbeave@gmail.com'
     def description = """\
 Incorporates GWT into Grails. In particular, GWT host pages can be
 GSPs and standard Grails services can be used to handle client RPC
 requests.
 """
 
-    def documentation = "http://simplicityitself.github.com/grails-gwt/guide/"
+    def documentation = 'http://simplicityitself.github.com/grails-gwt/guide/'
 
     def license = 'APACHE'
 
-    def pluginExcludes = [
-            'web-app/**'
+    def developers = [
+            [name: 'David Dawson', email: 'david.dawson@simplicityitself.com'],
+            [name: 'Peter Ledbrook', email: 'p.ledbrook@cacoethes.co.uk'],
+            [name: 'Alexey Zhokhov', email: 'donbeave@gmail.com']
     ]
 
     def srcDir = 'src/gwt'
 
-    def issueManagement = [system: 'GITHUB',
-                           url   : 'https://github.com/donbeave/grails-gwt/issues']
+    def issueManagement = [system: 'GitHub', url: 'https://github.com/donbeave/grails-gwt/issues']
     def scm = [url: 'https://github.com/donbeave/grails-gwt']
 
 }
