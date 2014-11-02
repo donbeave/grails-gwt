27bc9c4a5dd50d65a5b9c4c5cdde4c9c685f279c
Alexey Zhokhov <donbeave@gmail.com>
2014-11-02 18:12:08 +0800

    Added create-gwt-module script.

---
+++ src/templates/artifacts/Gwt.java
@@ -0,0 +1,16 @@
+package @artifact.package@;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class @artifact.name@ implements EntryPoint {
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+    }
+
+}
